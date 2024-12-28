54c73f30c7d64ee918048efd262e39cc8f2af6d5
ArmanGoodarzi <148266697+ArmanGoodarzi@users.noreply.github.com>
2024-12-28 14:05:17 +0330

    Practice3
    Fourth PDF

---
+++ Constructor&Destructor/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Constructor_Destructor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Car c = new Car(textBox1.Text,int.Parse(textBox2.Text));
+            
+        }
+    }
+    public class Car
+    {
+        private string brand { get; set; }
+        private int speed { get; set; }
+
+        public Car(string Brand , int Speed )
+        {
+            brand = Brand;
+            speed = Speed;
+        }
+        ~Car()
+        {
+            MessageBox.Show($"Brand : {brand},Speed : {speed}");   
+        }
+    }
+}
