0f9b4a192567744b09955a1f9a6ceb54d559d6f8
ArmanGoodarzi <148266697+ArmanGoodarzi@users.noreply.github.com>
2024-12-28 19:28:01 +0330

    Practice5
    Sixth PDF

---
+++ Polymorphism/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Polymorphism
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Vehicle car = new Car();
+            car.Drive();
+            Vehicle bike = new Bike();
+            bike.Drive();
+        }
+    }
+    public class Vehicle 
+    {
+        public virtual void Drive()
+        {
+            MessageBox.Show("Driving");
+        }
+    }
+    public class Car : Vehicle
+    {
+        public override void Drive()
+        {
+            MessageBox.Show("Driving fast");
+        }
+    }
+    public class Bike : Vehicle
+    {
+        public override void Drive()
+        {
+            MessageBox.Show("Pedal");
+        }
+    }
+}
